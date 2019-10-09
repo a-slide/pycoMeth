c9fb61b81cde6a8f8ee8df4f953c2e74192af648
a-slide <aleg@ebi.ac.uk>
2019-10-09 17:34:48 +0100

    Major Update of CLI with auto-documentation

--- pycoMeth/__main__.py
+++ pycoMeth/__main__.py
@@ -10,7 +10,8 @@ import sys
 from pycoMeth import __version__ as package_version
 from pycoMeth import __name__ as package_name
 from pycoMeth import __description__ as package_description
-from pycoMeth.Freq_meth_calculate import Freq_meth_calculate
+from pycoMeth.common import *
+from pycoMeth.Aggregate import Aggregate
 
 #~~~~~~~~~~~~~~TOP LEVEL ENTRY POINT~~~~~~~~~~~~~~#
 def main(args=None):
@@ -24,44 +25,26 @@ def main(args=None):
     subparsers = parser.add_subparsers (description="%(prog)s implements the following subcommands", dest="subcommands")
     subparsers.required = True
 
-    # Freq_meth_calculate subparser
-    subparser_fm = subparsers.add_parser("Freq_meth_calculate", description="Calculate methylation frequency at genomic CpG sites from the output of nanopolish call-methylation")
-    subparser_fm.set_defaults(func=Freq_meth_calculate_main)
-    subparser_fm_io = subparser_fm.add_argument_group("Input/Output options")
-    subparser_fm_io.add_argument("-i", "--input_fn", default=0, help="Path to a nanopolish call_methylation tsv output file. If not specified read from std input")
-    subparser_fm_io.add_argument("-b", "--output_bed_fn", type=str, default="", help="Path to write a summary result file in BED format")
-    subparser_fm_io.add_argument("-t", "--output_tsv_fn", type=str, default="", help="Path to write an more extensive result report in TSV format")
-    subparser_fm_fo = subparser_fm.add_argument_group("Filtering options")
-    subparser_fm_fo.add_argument("-d", "--min_depth", type=int, default=10, help="Minimal number of reads covering a site to be reported (default: %(default)s)")
-    subparser_fm_other = subparser_fm.add_argument_group("Other options")
-    subparser_fm_other.add_argument("-f", "--fasta_index", type=str, default="", help="fasta index file obtained with samtools faidx. Required for coordinate sorting")
-    subparser_fm_other.add_argument("-s", "--sample_id", type=str, default="", help="Sample ID to be used for the bed track header (default: %(default)s)")
-    subparser_fm_other.add_argument("--strand_specific", action="store_true", default=False, help="Output strand specific sites")
-    subparser_fm_other.add_argument("--min_llr", type=float, default=2, help="Minimal log likelyhood ratio to consider a site significantly methylated or unmethylated (default: %(default)s)")
+    # Aggregate subparser
+    f = Aggregate
+    sp_agg = subparsers.add_parser("Aggregate", description=doc_func(f))
+    sp_agg.set_defaults(func=f)
+    sp_agg_io = sp_agg.add_argument_group("Input/Output options")
+    add_argument(sp_agg_io, f, "input_fn", "i")
+    add_argument(sp_agg_io, f, "fasta_index", "f")
+    add_argument(sp_agg_io, f, "output_bed_fn", "b")
+    add_argument(sp_agg_io, f, "output_tsv_fn", "t")
+    sp_agg_ms = sp_agg.add_argument_group("Misc options")
+    add_argument(sp_agg_ms, f, "min_depth", "d")
+    add_argument(sp_agg_ms, f, "min_llr", "l")
+    add_argument(sp_agg_ms, f, "sample_id", "s")
 
     # Add common group parsers
-    for sp in [subparser_ec, subparser_fm]:
-        sp_verbosity = sp.add_mutually_exclusive_group()
-        sp_verbosity.add_argument("-v", "--verbose", action="store_true", default=False, help="Increase verbosity")
-        sp_verbosity.add_argument("-q", "--quiet", action="store_true", default=False, help="Reduce verbosity")
+    for sp in [sp_agg]:
+        sp_vb = sp.add_mutually_exclusive_group()
+        sp_vb.add_argument("-v", "--verbose", action="store_true", default=False, help="Increase verbosity")
+        sp_vb.add_argument("-q", "--quiet", action="store_true", default=False, help="Reduce verbosity")
 
     # Parse args and call subfunction
     args = parser.parse_args()
-    args.func(args)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SUBPARSERS FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-def Freq_meth_calculate_main (args):
-    """"""
-    # Run corresponding class
-    Freq_meth_calculate (
-        input_fn = args.input_fn,
-        fasta_index=args.fasta_index,
-        output_bed_fn = args.output_bed_fn,
-        output_tsv_fn = args.output_tsv_fn,
-        min_depth = args.min_depth,
-        sample_id = args.sample_id,
-        strand_specific = args.strand_specific,
-        min_llr = args.min_llr,
-        verbose = args.verbose,
-        quiet = args.quiet)
+    args.func(**vars(args))
