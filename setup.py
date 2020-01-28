e655962ab1b7860c5e1dfcf1c6cfc3b513efe227
a-slide <aleg@ebi.ac.uk>
2020-01-28 11:10:06 +0000

    Bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 # Define package info
 name = "pycoMeth"
-version = "0.3.1"
+version = "0.3.2"
 description = 'Python package for nanopore DNA methylation analysis downstream to Nanopolish'
 with open("README.md", "r") as fh:
     long_description = fh.read()
