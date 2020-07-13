9c20d8c492a48828a908143097a698fb096b796c
a-slide <aleg@ebi.ac.uk>
2020-07-13 16:11:55 +0200

    Bump up version number

--- pycoMeth/__init__.py
+++ pycoMeth/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = "0.4.11"
+__version__ = "0.4.12"
 __description__ = 'DNA methylation analysis downstream to Nanopolish for Oxford Nanopore DNA sequencing datasets'
